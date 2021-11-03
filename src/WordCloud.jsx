b5fd13cf3beedde38f2f7e15086e16f69edf6532
Tomasz Macisowicz <TomaszMacisowicz@users.noreply.github.com>
2021-11-03 13:40:51 +0100

    Upgrade to Mx9
    Upgrade dependencies.
    -react-d3-cloud.

--- src/WordCloud.jsx
+++ src/WordCloud.jsx
@@ -1,23 +1,34 @@
-import { Component, createElement } from "react";
+import React from 'react';
+import 'core-js';
+import { Component, createElement, render } from "react";
+
+
+
 import WordCloudWidgetComponent from "./components/WordCloudWidgetComponent";
 
-export default class WordCloudWidget extends Component {
 
-    shouldComponentUpdate(nextProps){
+export default class WordCloudWidget extends Component {
+    
+
+    shouldComponentUpdate(nextProps) {
+
         return nextProps.dataSource !== this.props.dataSource;
     }
 
     render() {
+
         var dataToRender = [];
-        if(this.props.dataSource.status === "available"){
-            var i=0;
-            for(i=0; i< this.props.dataSource.totalCount; i++) {
-                dataToRender.push({
-                    text: this.props.textAttrib(this.props.dataSource.items[i]).value, 
-                    value: this.props.intAttrib(this.props.dataSource.items[i]).value
+        
+        if (this.props.dataSource.status === "available" && this.props.dataSource.items) {            
+            var i = 0;
+                        for (i = 0; i < this.props.dataSource.items.length; i++) {
+                dataToRender.push({                                       
+                    text: this.props.textAttrib.get((this.props.dataSource.items[i])).value,
+                    value: this.props.intAttrib.get((this.props.dataSource.items[i])).value
                 });
             }
         }
         return <WordCloudWidgetComponent inputData={dataToRender} chosenWord={this.props.chosenWord} onClickAction={this.props.onClickAction}/>;
+
     }
 }
--- src/components/WordCloudWidgetComponent.jsx
+++ src/components/WordCloudWidgetComponent.jsx
@@ -1,20 +1,29 @@
-import { createElement } from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { Component, createElement, render } from "react";
+import 'core-js';
 import WordCloud from "react-d3-cloud";
 
-export default function WordCloudWidgetComponent(props){
+
+
+
+export default function WordCloudWidgetComponent(props) {
+  
   const data = props.inputData;
   const fontSizeMapper = word => Math.log2(word.value) * 5;
-  const rotate = word => (Math.floor(Math.random()*2) * 90);
-
+  const rotate = word => (Math.floor(Math.random() * 2) * 90);
   const onClickHandle = (text) => {
-    if (props.onClickAction.canExecute && !props.onClickAction.isExecuting) {
-        props.chosenWord.setValue(text);
-        props.onClickAction.execute();
+    
+    (props.onClickAction.canExecute && !props.onClickAction.isExecuting)
+    
+    {
+      props.chosenWord.setValue(text);
+      props.onClickAction.execute();
     }
   };
-  
+
   return (
-    <WordCloud data={data} fontSizeMapper={fontSizeMapper} rotate={rotate} onWordClick={word => onClickHandle(word.text)}/>
+
+    <WordCloud data={data} fontSizeMapper={fontSizeMapper} rotate={rotate} onWordClick={(event, word) => onClickHandle(word.text)} />
+
   );
 }
